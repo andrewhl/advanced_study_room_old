0d7c5661f14890e7762d11d016d552beb2eaaad8
Andrew Hlavats <ahlavats@rogers.com>
2011-11-30 19:35:12 -0500

    Added helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,13 @@ module ApplicationHelper
   def sub_title
     "#{@title}"
   end
+  
+  def url_get  
+    require 'net/http'
+    result = Net::HTTP.get(URI.parse('http://www.gokgs.com/gameArchives.jsp?user=andrew'))
+    puts result
+    
+    require 'open-uri'
+    puts open("http://www.gokgs.com/gameArchives.jsp?user=andrew").read
+  end
 end
