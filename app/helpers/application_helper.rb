73c1b20ac6f3d07b16ec5598fecd3c65eb6fee77
Andrew Hlavats <ahlavats@rogers.com>
2011-12-05 22:04:08 -0500

    Byo-yomi parsing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,58 +12,7 @@ module ApplicationHelper
   def sub_title
     "#{@title}"
   end
-    
-  def nokogiri_get 
-    
-    require 'open-uri'
-    require 'time'
-    doc = Nokogiri::HTML(open("http://www.gokgs.com/gameArchives.jsp?user=andrew&year=2011&month=11"))
-    doc = doc.xpath('//table[1]')
-    links = doc.css('tr > td[1] > a').map { |link| link[:href] }
-    puts links
-    white_player = doc.css('tr > td[2] > a').each do |stuff|
-      white_player_name = stuff.content.scan(/^\w+/)
-      white_player_rank = stuff.content.scan(/\?|-|[1-9]?[1-9][kdp]/) # Regex for KGS ranks
-      test2 = rank_convert(white_player_rank)
-      puts test2
-      puts "#{white_player_name}, #{white_player_rank}"
-    end
-    black_player = doc.css('tr > td[3] > a').each do |stuff|
-      black_player_name = stuff.content.scan(/^\w+/)
-      black_player_rank = stuff.content.scan(/\?|-|[1-9]?[1-9][kdp]/) # Regex for KGS ranks
-      test = rank_convert(black_player_rank)
-      puts test
-      puts "#{black_player_name}, #{black_player_rank}"
-    end
-    board_size_and_handicap = doc.css('tr > td[4]').each do |stuff|
-      boardArray = stuff.content.scan(/[0-9]+/)
-      board_size = boardArray[0]
-	  
-	  if boardArray.length() == 3
-	    handi = boardArray[2]
-	  else
-	    handi = 0
-	  end
-	  print "Board Size: ", board_size, "\n"
-	  print "Handicap: ", handi, "\n"
-	  
-    end
-    date = doc.css('tr > td[5]').each do |stuff|
-      stuff = stuff.content
-      time = DateTime.strptime(stuff, "%m/%d/%Y %I:%M %p").utc.to_time.to_i * -1
-      puts time
-    end
-    game_type = doc.css('tr > td[6]').each do |stuff|
-      unless stuff.content.include?("Rengo")
-        puts stuff.content
-      end
-    end
-    result = doc.css('tr > td[7]').each do |stuff|
-      puts stuff.content
-    end
-
-  end
-  
+      
   def rank_convert(rank)
     rank = rank[0]
     if rank[-1,1] == "d"
@@ -83,41 +32,8 @@ module ApplicationHelper
       puts "I DID NOT KNOW WHAT TO DO."
     end
   end
-  
-  def sgf_parse
     
-     # sgf = KantanSgf::Sgf.new('public/casio-clement44.sgf')
-     # sgf.parse
-     # 
-     #  sgf.comments.each do |key, value|
-     #     if value.scan(/ASR League/i)
-     #       valid = true
-     #       return valid
-     #     else
-     #       valid = false
-     #       return valid
-     #     end
-     #  end
-      
-      require 'sgf'
-      sgf = SGF::Parser.new(row["url"])
-      sgf = KantanSgf::Sgf.new(row["url"])
-      sgf.parse
-      
-      komi = sgf.komi
-      
-      sgf.comments.each do |key, value|
-        if value.scan(/ASR League/i)
-          valid = true
-        else
-          valid = false
-          next
-        end
-      end
-    
-  end
-  
-  def nokogiri_test
+  def match_scraper
     
     require 'open-uri'
     require 'time'
@@ -190,14 +106,13 @@ module ApplicationHelper
 
     end
     
+    # Various filters
     for row in games
       if row["public_game"] == "No"
         next
       elsif row["board_size"] != 19
         next
       elsif row["game_type"] == "Rengo"
-        next
-      elsif row["game_type"] == "Simul"
         next
       elsif row["game_type"] == "Teaching"
         next
@@ -215,8 +130,53 @@ module ApplicationHelper
         tree = parser.parse sgf_raw
         
         game_info = tree.root.children[0].properties
-        komi = game_info["KM"][1..-2].to_f 
+        
+        # Check that over time is at least 5x30 byo-yomi
+        over_time = game_info["OT"]
+        if over_time == nil
+          next
+        end
+        
+        # Omit games with the Canadian ruleset
+        if over_time["Canadian"]
+          next
+        end
+        
+        # Restrict overtime settings
+        over_time = over_time.split(' ')
+        byo_yomi_periods = over_time[0].split('x')[0].to_i # Parse SGF overtime periods
+        byo_yomi_stones = over_time[0].split('x')[1].to_i # Parse SGF overtime stones
+        
+        if byo_yomi_periods < 5
+          next
+        end
+        
+        if (byo_yomi_periods == 5) and (byo_yomi_stones < 30)
+          next
+        end
+        
+        # Check main time is between 1500 and 2700 seconds
+        main_time = game_info["TM"].to_i
+        
+        if main_time < 1500
+          next
+        end        
+        
+        # Check ruleset is Japanese
+        ruleset = game_info["RU"]
+        
+        if ruleset != "Japanese"
+          next
+        end
+        
+        # Check komi is 6.5
+        komi = game_info["KM"][1..-2].to_f
+        
+        if komi != 6.5
+          next
+        end
 
+        # Confirm 'ASR League' is mentioned within first 30 moves
         game = tree.root
         valid = false
         for i in 0..30
@@ -230,7 +190,9 @@ module ApplicationHelper
         if valid != true
           next
         end
-        rowadd = Match.new(:url => row["url"], :white_player_name => row["white_player_name"], :white_player_rank => row["white_player_rank"], :black_player_name => row["black_player_name"], :black_player_rank => row["black_player_rank"], :result_boolean => row["result_boolean"], :score => row["score"], :board_size => row["board_size"], :handi => row["handi"], :unixtime => row["unixtime"], :game_type => row["game_type"], :komi => komi, :valid => valid, :result => row["result"])
+        
+        # Submit game to DB
+        rowadd = Match.new(:url => row["url"], :white_player_name => row["white_player_name"], :white_player_rank => row["white_player_rank"], :black_player_name => row["black_player_name"], :black_player_rank => row["black_player_rank"], :result_boolean => row["result_boolean"], :score => row["score"], :board_size => row["board_size"], :handi => row["handi"], :unixtime => row["unixtime"], :game_type => row["game_type"], :ruleset => ruleset, :komi => komi, :valid => valid, :result => row["result"])
         rowadd.save
       end
     end    
