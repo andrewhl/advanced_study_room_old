81804d7c3a962a242be393c0de546c60a9cdfd8f
Andrew Hlavats <ahlavats@rogers.com>
2011-12-15 14:31:00 -0500

    More changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,19 @@ class KGSValidator < ActiveModel::Validator
       record.errors[:kgs_names] << ("The KGS account you entered does not exist. Please ensure that your name matches the KGS name exactly.")
       return
     end
-
-    a = doc.first.css('td a')[0].content.scan(/(\w+)/)
-    b = doc.first.css('td a')[1].content.scan(/(\w+)/)
+    
+    myRegex =  /(\w+) \[(\?|-|\w+)\??\]/
+    
+    a = doc.first.css('td')[1].content.scan(myRegex)
+    b = doc.first.css('td')[2].content.scan(myRegex)
 
     # Politely changes the name to the correct version we scraped from the site.
     if a[0][0].casecmp(name) == 0
       record.kgs_names = a[0][0]
+      record.rank = rank_convert(a[0][1])
     elsif b[0][0].casecmp(name) == 0
       record.kgs_names = b[0][0]
+      record.rank = rank_convert(b[0][1])
     else
       # Errors if we can't verify the account due to lack of games this month.
       record.errors[:kgs_names] << ("The KGS account you entered has not played any games lately, and we were unable to verify it. Please make certain you have at least one game, review, or demo this month.")
--- lib/tasks/user_tasks.rake
+++ lib/tasks/user_tasks.rake
@@ -3,11 +3,16 @@ task :signup_users => :environment do
 
   usernames = ["shinichi56", "bartimeus", "compgo74", "faceless1", "deathwind", "drgoplayer", "plusguy", "fab", "jozles", "joenosai", "geser", "eddy", "baddack", "lonelyjoy", "joeseki", "katchumo", "ikeike", "teach", "niark", "tanushka", "drogba987", "bert", "mo", "benjii", "gabalon", "ciaso", "jubilex", "thecmvick", "boule2", "thesirjay", "fern", "gryn", "dmitriy81", "startagain", "merula", "edisson", "latios", "tlapeg07", "hellhuman", "ocrilim", "nannyogg", "turingtest", "orunandaka", "moichido", "ddusopp", "freehold", "sleazywiki", "dreamc", "agzam", "lemike", "schrody", "ryuu", "usagi", "gaechka", "helloqtion", "fran6", "jtscarry", "senyuki"]
   
-  for x in usernames
+  usernames.each do |x|
+    begin
     sleep(12)
     puts "Saving #{x}"
     @user = User.new(:username => x, :kgs_names => x, :email => "#{x}@test.com", :password => "testtest", :password_confirmation => "testtest")
     @user.save!
+    
+    rescue
+      next
+    end
   end
 end
   
